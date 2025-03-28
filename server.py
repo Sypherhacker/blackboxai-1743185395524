d5d1e7d6945b6e955d8492908ab1dbf9f4eaa4c2
Sypherhacker <chinmaysande424@gmail.com>
2025-03-28 23:39:55 +0530

    Add server.py 1743185397840

---
+++ server.py
@@ -0,0 +1,24 @@
+from http.server import SimpleHTTPRequestHandler
+from socketserver import TCPServer
+import os
+
+PORT = 8000
+DIRECTORY = "."
+
+class Handler(SimpleHTTPRequestHandler):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, directory=DIRECTORY, **kwargs)
+
+    def end_headers(self):
+        self.send_header('Cache-Control', 'no-store, no-cache, must-revalidate')
+        self.send_header('Pragma', 'no-cache')
+        self.send_header('Expires', '0')
+        super().end_headers()
+
+if __name__ == "__main__":
+    with TCPServer(("", PORT), Handler) as httpd:
+        print(f"Serving at http://localhost:{PORT}")
+        try:
+            httpd.serve_forever()
+        except KeyboardInterrupt:
+            print("\nServer stopped")
